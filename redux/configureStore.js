43603e5c3b2e8df0f7f8fb88c6a1ff740621e0cc
Jordy <adityakumawat51@gmail.com>
2019-10-11 10:37:33 +0530

    Redux Adding Favorites

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -4,15 +4,17 @@ import logger from 'redux-logger';
 import { dishes } from './dishes';
 import { leaders } from './leaders';
 import { comments } from './comments';
+import { favorites } from './favorites';
 import { promotions } from './promotions';
 
 export const ConfigureStore = () => {
     const store = createStore(
         combineReducers({
             dishes,
+            leaders,
             comments,
-            promotions,
-            leaders
+            favorites,
+            promotions
         }),
         applyMiddleware(thunk, logger)
     )
